6b536678429a40c01f99b3c36ddf50458f946ace
Froilan Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-07-19 15:56:14 +0000

    feat(checkbox): implement checkbox component with template, styles, and reactive form support

--- src/app/shared/components/checkbox/checkbox.ts
+++ src/app/shared/components/checkbox/checkbox.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CheckboxModule } from "primeng/checkbox";
 
 @Component({
-  selector: 'app-checkbox',
-  imports: [],
-  templateUrl: './checkbox.html',
-  styleUrl: './checkbox.css'
+    selector: "app-checkbox",
+    imports: [CheckboxModule, FormsModule],
+    templateUrl: "./checkbox.html",
+    styleUrl: "./checkbox.css",
 })
 export class Checkbox {
-
+    _checkbox: boolean = false;
+    _label: string = "Checkbox";
+    _disabled: boolean = false;
+    _required: boolean = false;
+    _variant: "filled" | "outlined" = "filled";
+    _size: "small" | "large" = "small";
+    _invalid: boolean = false;
+    _name: string = "checkbox";
+    _id: string = "checkboxField";
 }
