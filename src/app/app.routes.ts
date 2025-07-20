12430f70eb299ca8bb7e08b4ed9c9b2e51f59d9f
Froilan J Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-07-20 18:51:18 +0800

    feat(routes): restructure routing for sign-in component and update app structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,17 @@ import { Routes } from "@angular/router";
 import { SignIn } from "@features/auth/sign-in/sign-in";
 export const routes: Routes = [
     {
-        path: "flow/auth/signin",
-        component: SignIn,
-        title: "Synectix | Sign In",
+        path: "flow",
+        children: [
+            {
+                path: "auth",
+                children: [
+                    {
+                        path: "signin",
+                        component: SignIn,
+                    },
+                ],
+            },
+        ],
     },
 ];
