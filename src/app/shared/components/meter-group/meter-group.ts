334ca922c1daaf3d455e041ac1558f311080da11
Froilan Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-07-20 02:03:23 +0000

    feat(meter-group): add meter group component with template, styles, and unit tests

---
+++ src/app/shared/components/meter-group/meter-group.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { MeterGroupModule } from "primeng/metergroup";
+@Component({
+    selector: "app-meter-group",
+    imports: [],
+    templateUrl: "./meter-group.html",
+    styleUrl: "./meter-group.css",
+})
+export class MeterGroup {}
