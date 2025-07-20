1b2a3bb9b6fd5da8888ae53cdd6e6d5a65c91410
Froilan J Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-07-20 15:34:41 +0800

    feat(input): extend input component with base form field functionality and value handling

--- src/app/shared/components/radio/radio.ts
+++ src/app/shared/components/radio/radio.ts
@@ -1,11 +1,18 @@
-import { Component } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { Component, forwardRef } from "@angular/core";
+import { FormsModule, NG_VALUE_ACCESSOR } from "@angular/forms";
 import { RadioButtonModule } from "primeng/radiobutton";
 @Component({
     selector: "app-radio",
     imports: [RadioButtonModule, FormsModule],
     templateUrl: "./radio.html",
     styleUrl: "./radio.css",
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => Radio),
+            multi: true,
+        },
+    ],
 })
 export class Radio {
     _radio: boolean = false;
@@ -16,5 +23,5 @@ export class Radio {
     _size: "small" | "large" = "small";
     _invalid: boolean = false;
     _name: string = "radio";
-    _id: string = "radioField";
+    _id: string = Math.random().toString(36).substring(2);
 }
