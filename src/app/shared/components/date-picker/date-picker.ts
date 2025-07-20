6975e338937cceea74421d59e1b504439d8efbbd
Froilan J Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-07-20 13:11:05 +0800

    feat(date-picker): implement date-picker component with inputs for configuration

--- src/app/shared/components/date-picker/date-picker.ts
+++ src/app/shared/components/date-picker/date-picker.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { DatePickerModule } from "primeng/datepicker";
 @Component({
@@ -7,4 +7,19 @@ import { DatePickerModule } from "primeng/datepicker";
     templateUrl: "./date-picker.html",
     styleUrl: "./date-picker.css",
 })
-export class DatePicker {}
+export class DatePicker {
+    @Input() min = new Date(1900, 0, 1);
+    @Input() max = new Date(2100, 11, 31);
+    @Input() selectionMode: "single" | "multiple" | "range" = "single";
+    @Input() showButtonBar = false;
+    @Input() showTime = false;
+    @Input() timeFormat: "datetime12h" | "datetime24h" = "datetime12h";
+    @Input() view: "date" | "month" | "year" = "date";
+    @Input() numberOfMonths = 1;
+    @Input() inline = false;
+    @Input() showWeek = false;
+    @Input() size: "small" | "medium" | "large" = "medium";
+    @Input() variant: "filled" | "outlined" | "ghost" = "filled";
+    @Input() invalid = false;
+    @Input() customClass: string = "";
+}
