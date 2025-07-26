439bab48e6524f293d75b0f4f1d7e1eb8ee8d826
Froilan J Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-07-26 23:40:19 +0800

    feat: add HttpService for handling HTTP requests and update environment configuration

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: 'http://127.0.0.1:8080/api',
+    featureToggle: {
+        enableChat: true,
+        enablePayments: false
+    }
+}
