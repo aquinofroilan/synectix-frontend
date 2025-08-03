88034c88494ad8a80d8e2516498b899e520290ec
Froilan J Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-08-03 16:26:58 +0800

    feat: update routing titles for home, sign in, and sign up pages; remove unused italic font file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,7 @@ import { Routes } from "@angular/router";
 export const routes: Routes = [
     {
         path: "",
-        redirectTo: "/home",
-        pathMatch: "full",
-    },
-    {
-        path: "home",
+        title: "Synectix | Home",
         loadComponent: () => import("@features/homepage/homepage").then((m) => m.Homepage),
     },
     {
@@ -16,10 +12,12 @@ export const routes: Routes = [
                 path: "auth",
                 children: [
                     {
+                        title: "Synectix | Sign In",
                         path: "signin",
                         loadComponent: () => import("@features/auth/signin/signin").then((m) => m.SignIn),
                     },
                     {
+                        title: "Synectix | Sign Up",
                         path: "signup",
                         loadComponent: () => import("@features/auth/signup/signup").then((m) => m.Signup),
                     },
