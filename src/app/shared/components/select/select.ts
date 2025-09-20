47bcdf9caf9629e2f678950fbf276cb79b3f8827
Froilan Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-09-20 23:52:01 +0800

    feat: implement new hlm-select component with enhanced structure and styling, update select imports and adjust signup layout

--- src/app/shared/components/select/select.ts
+++ src/app/shared/components/select/select.ts
@@ -1,11 +1,13 @@
-import { Component, forwardRef, Input } from "@angular/core";
-import { FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule, type ControlValueAccessor } from "@angular/forms";
-import { BaseFormField } from "@shared/base/form-field/base-form-field";
-import { SelectModule } from "primeng/select";
+import {Component, forwardRef, Input} from "@angular/core";
+import {type ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule} from "@angular/forms";
+import {BaseFormField} from "@shared/base/form-field/base-form-field";
+import {SelectModule} from "primeng/select";
+import {BrnSelectImports} from "@spartan-ng/brain/select";
+import {HlmSelectImports} from "@spartan-ng/helm/select";
 
 @Component({
     selector: "app-select",
-    imports: [SelectModule, ReactiveFormsModule, FormsModule],
+    imports: [SelectModule, ReactiveFormsModule, FormsModule, BrnSelectImports, HlmSelectImports],
     template: `
         <section [class]="customClass">
             <label class="text-sm" [htmlFor]="inputId">{{ label }}</label>
@@ -24,7 +26,17 @@ import { SelectModule } from "primeng/select";
                 [disabled]="_disabled"
                 [inputId]="inputId"
             />
-            <small>{{ helperText }}</small>
+            <brn-select class="inline-block" placeholder="Select an option">
+                <hlm-select-trigger>
+                    <hlm-select-value />
+                </hlm-select-trigger>
+                <hlm-select-content class="w-56">
+                    <hlm-option value="Refresh">Refresh</hlm-option>
+                    <hlm-option value="Settings">Settings</hlm-option>
+                    <hlm-option value="Help">Help</hlm-option>
+                    <hlm-option value="Signout">Sign out</hlm-option>
+                </hlm-select-content>
+            </brn-select>
         </section>
     `,
     providers: [
